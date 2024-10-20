b585e5206088cb4613873d389429af366f54726a
Justin Morrison <morrisonak@yahoo.com>
2024-10-20 14:50:52 -0500

    update theme.server.ts

--- app/lib/theme.server.ts
+++ app/lib/theme.server.ts
@@ -14,10 +14,8 @@ export function getTheme(request: Request): Theme | null {
   return null;
 }
 
-export function setTheme(theme: Theme | "system") {
-  if (theme === "system") {
-    return cookie.serialize(cookieName, "", { path: "/", maxAge: -1 });
-  } else {
-    return cookie.serialize(cookieName, theme, { path: "/", maxAge: 31536000 });
-  }
+export function setTheme(theme: Theme) {
+  // Serialize and set the cookie with the selected theme
+  return cookie.serialize(cookieName, theme, { path: "/", maxAge: 31536000 });
 }
+
