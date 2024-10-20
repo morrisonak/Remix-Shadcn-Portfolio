c52ae6d59ea53b6c938c2a3a9f6d4b61c3eb5c79
Justin Morrison <morrisonak@yahoo.com>
2024-10-20 16:10:06 -0500

    update nav links

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Link } from "@remix-run/react";
 import * as React from "react";
 import { Button } from "~/components/ui/button";
@@ -16,13 +14,13 @@ export function Nav() {
   const [isOpen, setIsOpen] = React.useState(false);
 
   const handleLinkClick = () => {
-    setIsOpen(false);
+    setIsOpen(false); // Close the sheet on link click
   };
 
   return (
     <header className="flex items-center justify-between px-4 py-2 md:py-4">
       <div className="flex items-center space-x-4">
-        <Link className="flex items-center space-x-2" to="/">
+        <Link className="flex items-center space-x-2" to="/" preventScrollReset>
           <h1 className="text-2xl font-bold">
             {constants.firstLastname}, {constants.professionalTitle}
           </h1>
@@ -49,45 +47,30 @@ export function Nav() {
                 d="M4 6h16M4 12h16M4 18h16"
               />
             </svg>
-            <span className="sr-only">Menu</span>
+            <span className="sr-only">Links</span>
           </Button>
         </SheetTrigger>
         <SheetContent side="right" className="w-[300px] sm:w-[400px]">
           <SheetHeader>
-            <SheetTitle className="text-xl font-bold">Menu</SheetTitle>
+            <SheetTitle className="text-xl font-bold">Links</SheetTitle>
           </SheetHeader>
           <nav className="flex flex-col space-y-4 mt-4">
             <Link
               to="/"
               className="text-gray-800 hover:text-blue-600"
               onClick={handleLinkClick}
+              preventScrollReset
             >
               Home
             </Link>
             <Link
-              to="/about"
+              to="https://github.com/morrisonak/Remix-Shadcn-Portfolio"
               className="text-gray-800 hover:text-blue-600"
               onClick={handleLinkClick}
             >
-              About
+              Github Repo
             </Link>
-            <Link
-              to="/services"
-              className="text-gray-800 hover:text-blue-600"
-              onClick={handleLinkClick}
-            >
-              Services
-            </Link>
-            {/* <Link to="/contact" className="text-gray-800 hover:text-blue-600" onClick={handleLinkClick}>
-              Contact
-            </Link> */}
           </nav>
-
-          {/* <img
-            src="/WDC-2.png"
-            alt="Wayside Design Consultants Logo"
-            className="h-48 w-auto mx-auto mt-4 mb-4"
-          /> */}
         </SheetContent>
       </Sheet>
     </header>
--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,42 +7,18 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  //json,
 } from "@remix-run/react";
 import styles from "./tailwind.css";
-//import { getTheme } from "./lib/theme.server";
-// import {
-//   ClientHintCheck,
-//   getHints,
-//   useNonce,
-//   useTheme,
-// } from "./lib/client-hints";
-//import clsx from "clsx";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
-// export const loader = async ({ request }: LoaderFunctionArgs) => {
-//   return json({
-//     requestInfo: {
-//       hints: getHints(request),
-//       userPrefs: {
-//         theme: getTheme(request),
-//       },
-//     },
-//   });
-// };
-
 export default function App() {
-  // const theme = useTheme();
-  // const nonce = useNonce();
-
   return (
     <html lang="en">
       <head>
-     
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
@@ -50,7 +26,6 @@ export default function App() {
       </head>
       <body>
         <Outlet />
-
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
